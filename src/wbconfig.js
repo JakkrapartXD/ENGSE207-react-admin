40f38817901c61527156707fb8aa460bfef68c57
JakkrapartXD <jakkrapart.x78@gmail.com>
2024-09-07 01:12:22 +0700

    add wallboard

---
+++ src/wbconfig.js
@@ -0,0 +1,21 @@
+const wbconfig = {
+    development: {
+        hosturl  : 'https://10.21.43.203:8405/api',
+        wsurl    : 'wss://10.21.43.203:8405.net',
+        masterKey     : 'wallboardapi',
+        clientKey     : 'wallboardapi',
+        javascriptKey : 'wallboardapi',
+        appId : "wallboardapi"
+    },
+    production: {
+        hosturl  : 'https://10.21.43.203:8405/api',
+        wsurl    : 'wss://10.21.43.203:8405',
+        masterKey     : 'wallboardapi',
+        clientKey     : 'wallboardapi',
+        javascriptKey : 'wallboardapi',
+        appId : "wallboardapi"
+    }
+};
+export default wbconfig;
+
+
